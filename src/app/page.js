8cde79af7b3cc7159bdad04636635844fccae6da
Albin <albinanto06@gmail.com>
2025-02-18 12:02:41 +0530

    index page routed to admin-dashboard/manage-company

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,15 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
+
 export default function Home() {
+
+
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/admin-dashboard/manage-company")
+  }, []);
+
   return (
     <div className="">
       <div>Home</div>
